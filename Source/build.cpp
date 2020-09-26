e2ae37be0a5b45f2082fbd632c42611bb8dc265b
Tailon <tailrusse2020@gmail.com>
2020-09-25 23:57:32 -0800

    Passing almost half the tests; just passed the 2 bridge reordered tests

--- Source/build.cpp
+++ Source/build.cpp
@@ -7,14 +7,18 @@ int build(int w, int e, const std::vector<Bridge> &bridges) {
     if (bridges.size() == 1) {
         return bridges[0][2];
     } else if (bridges.size() == 2) {
-        if (!doBridgesCross(bridges[0], bridges[1])) {
+        if(bridges[0][0] == bridges[1][0] || bridges[0][1] == bridges[1][1]) {
             return std::max(bridges[0][2], bridges[1][2]);
+        } else if (!doBridgesCross(bridges[0], bridges[1])) {
+            return bridges[0][2] + bridges[1][2];
         }
-    }else {
+        return std::max(bridges[0][2], bridges[1][2]);
+    } else {
         return 0;
     }
 }
 
 bool doBridgesCross(const Bridge &first, const Bridge &second) {
-    return true;
+    return (first[0] < second[0] && first[1] > second[1]) ||
+            (first[0] > second[0] && first[1] < second[1]);
 }
