59e00ffb6ad2d968ef81eebf72cc2fb4e334b1a0
Tailon <tailrusse2020@gmail.com>
2020-09-25 21:55:01 -0800

    Passing some tests; working on test case 2 bridges non-crossing

--- Header/build.h
+++ Header/build.h
@@ -8,5 +8,5 @@
 #include <vector>
 using Bridge = std::vector<int>;
 int build(int w, int e, const std::vector<Bridge> &bridges);
-
+bool doBridgesCross(const Bridge &first, const Bridge &second);
 #endif //HOMEWORK2_BUILD_H
--- Source/build.cpp
+++ Source/build.cpp
@@ -4,5 +4,17 @@
 
 #include "../Header/build.h"
 int build(int w, int e, const std::vector<Bridge> &bridges) {
-    return 0;
+    if (bridges.size() == 1) {
+        return bridges[0][2];
+    } else if (bridges.size() == 2) {
+        if (!doBridgesCross(bridges[0], bridges[1])) {
+            return std::max(bridges[0][2], bridges[1][2]);
+        }
+    }else {
+        return 0;
+    }
+}
+
+bool doBridgesCross(const Bridge &first, const Bridge &second) {
+    return true;
 }
